a3e686db67d02e8d3899c15ca72fcb683e0c2c40
Bohdan566 <bohdan55566@gmail.com>
2020-10-29 21:30:19 +0200

    Lesso1

---
+++ JS_HomeWork_1.js
@@ -0,0 +1,338 @@
+
+// Завдання_1 
+    // let string1_1 = "Hello";
+    // let string1_2 = "OWU";
+    // let string1_3 = "UA";
+    // let number1_1 = 1;
+    // let number1_2 = 10;
+    // let number1_3 = -999;
+    // let number1_4 = 123;
+    // let number1_5 = 3.14;
+    // let number1_6 = 2.7;
+    // let number1_7 = 16;
+    // let bulean1_1 = true;
+    // let bulean1_2 = false;
+    // console.log(string1_1);
+    // console.log(string1_2);
+    // console.log(string1_3);
+    // console.log(number1_1);
+    // console.log(number1_2);
+    // console.log(number1_3);
+    // console.log(number1_4);
+    // console.log(number1_5);
+    // console.log(number1_6);
+    // console.log(number1_7);
+    // console.log(bulean1_1);
+    // console.log(bulean1_2);
+    // alert(string1_1);
+    // alert(string1_2);
+    // alert(string1_3);
+    // alert(number1_1);
+    // alert(number1_2);
+    // alert(number1_3);
+    // alert(number1_4);
+    // alert(number1_5);
+    // alert(number1_6);
+    // alert(number1_7);
+    // alert(bulean1_1);
+    // alert(bulean1_2);
+    // document.write(string1_1);
+    // document.write(string1_2);
+    // document.write(string1_3);
+    // document.write(number1_1);
+    // document.write(number1_2);
+    // document.write(number1_3);
+    // document.write(number1_4);
+    // document.write(number1_5);
+    // document.write(number1_6);
+    // document.write(number1_7);
+    // document.write(bulean1_1);
+    // document.write(bulean1_2);
+
+
+// Завдання_2 
+
+    // string1_1 = "Hello World";   
+    // string1_2 = "OWU 2020";
+    // string1_3 = "UA 2020";
+
+    // number1_1 = 100;
+    // number1_2 = 1000;
+    // number1_3 = -99900;
+    // number1_4 = 12300;
+    // number1_5 = 3.1411;
+    // number1_6 = 2.711;
+    // number1_7 = 1600;
+
+    // bulean1_1 = true;
+    // bulean1_2 = false;
+
+    // console.log(string1_1);
+    // console.log(string1_2);
+    // console.log(string1_3);
+
+    // console.log(number1_1);
+    // console.log(number1_2);
+    // console.log(number1_3);
+    // console.log(number1_4);
+    // console.log(number1_5);
+    // console.log(number1_6);
+    // console.log(number1_7);
+
+    // console.log(bulean1_1);
+    // console.log(bulean1_2);
+
+
+    // alert(string1_1);
+    // alert(string1_2);
+    // alert(string1_3);
+
+    // alert(number1_1);
+    // alert(number1_2);
+    // alert(number1_3);
+    // alert(number1_4);
+    // alert(number1_5);
+    // alert(number1_6);
+    // alert(number1_7);
+
+    // alert(bulean1_1);
+    // alert(bulean1_2);
+
+
+    // document.write(string1_1);
+    // document.write(string1_2);
+    // document.write(string1_3);
+
+    // document.write(number1_1);
+    // document.write(number1_2);
+    // document.write(number1_3);
+    // document.write(number1_4);
+    // document.write(number1_5);
+    // document.write(number1_6);
+    // document.write(number1_7);
+
+    // document.write(bulean1_1);
+    // document.write(bulean1_2);
+
+
+
+// Завдання_3 
+
+// const string2_1 = "Bohdan";
+// const string2_2 = "Vasya";
+// const string2_3 = "Andriy";
+
+// const number2_1 = 1994;
+// const number2_2 = 1999;
+// const number2_3 = 1991;
+
+// console.log(string2_1);
+// console.log(string2_2);
+// console.log(string2_3);
+
+// console.log(number2_1);
+// console.log(number2_2);
+// console.log(number2_3);
+
+
+// alert(string2_1);
+// alert(string2_2);
+// alert(string2_3);
+
+// alert(number2_1);
+// alert(number2_2);
+// alert(number2_3);
+
+// document.write(string2_1);
+// document.write(string2_2);
+// document.write(string2_3);
+
+// document.write(number2_1);
+// document.write(number2_2);
+// document.write(number2_3);
+
+
+// Завдання_4 
+
+// let firstName = prompt("What is your name?");
+// let lastName = prompt("What is your last name?");
+// let surName = prompt("What is your ser name?");
+
+// alert(firstName);
+// alert(lastName);
+// alert(surName);
+
+// console.log(firstName);
+// console.log(lastName);
+// console.log(surName);
+
+// document.write(firstName);
+// document.write(lastName);
+// document.write(surName);
+
+
+
+// Завдання_5
+
+// let person = firstName += lastName += surName;
+
+// alert(person);
+
+// Завдання_6
+
+// let firstName1 = prompt("What is your firstname?");
+// let lastName1 = prompt("What is your last name?");
+// let surName1 = prompt("What is your ser name?");
+
+// let firstName2 = prompt("What is your firstname?");
+// let lastName2 = prompt("What is your last name?");
+// let surName2 = prompt("What is your ser name?");
+
+// let Katie = firstName1 += lastName1 += surName1;
+// let Oksana = firstName2 += lastName2 += surName2;
+
+// alert(Katie);
+// alert(Oksana);
+
+// Завдання_7
+
+// let numb_1 = +prompt("Введіть перше число");
+// let numb_2 = +prompt("Введіть друге число");
+// let numb_3 = +prompt("Введіть третє число");
+
+// console.log(numb_1);
+// console.log(numb_2);
+// console.log(numb_3);
+
+// Завдання_8
+
+// let numb_1 = parseInt(prompt("Введіть перше число"));
+// let numb_2 = parseInt(prompt("Введіть друге число"));
+// let numb_3 = parseInt(prompt("Введіть третє число"));
+// let numb_4 = parseInt(prompt("Введіть четверте число"));
+// let result = numb_1 + numb_2 + numb_3 + numb_4;
+
+// console.log(numb_1);
+// console.log(numb_2);
+// console.log(numb_3);
+// console.log(numb_4);
+// console.log(result);
+
+// Завдання_9
+
+
+// let numb_1 = Number.parseFloat(prompt("Введіть перше число"));
+// let numb_2 = Number.parseFloat(prompt("Введіть друге число"));
+// let numb_3 = Number.parseFloat(prompt("Введіть третє число"));
+// let result = numb_1 + numb_2 + numb_3;
+
+
+
+// console.log(numb_1);
+// console.log(numb_2);
+// console.log(numb_3);
+// console.log(result);
+
+
+// Завдання_10
+
+// let numb_1 = Math.round(prompt("Введіть перше число"));
+// let numb_2 = Math.round(prompt("Введіть друге число"));
+// let numb_3 = Math.round(prompt("Введіть третє число"));
+// let result = numb_1 + numb_2 + numb_3;
+
+// console.log(numb_1);
+// console.log(numb_2);
+// console.log(numb_3);
+// console.log(result);
+
+// Завдання_11
+
+// let numb_1 = parseInt(prompt("Введіть перше число"));
+// let numb_2 = parseInt(prompt("Введіть друге число"));
+// let result = Math.pow(numb_1, numb_2);
+
+// console.log(numb_1);
+// console.log(numb_2);
+// console.log(result);
+
+// Завдання_12
+
+// let a = 100;
+// let b = "100";
+// let c = true;
+// let d = undefined;
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+
+// Завдання_13
+
+// let a1 = 5 < 6;
+// let a2 = 5 > 6;
+// let a3 = 5 >= 6;
+// let a4 = 5 == 6;
+// let a5 = 10 == 10;
+// let a6 = 10 >= 10;
+// let a7 = 10 < 10;
+// let a8 = 10 > 10;
+// let a9 = 10 != 10;
+// let a10 = 123 === "123";
+// let a11 = 123 == "123";
+
+
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+// console.log(a6);
+// console.log(a7);
+// console.log(a8);
+// console.log(a9);
+// console.log(a10);
+// console.log(a11);
+
+// Завдання_14
+
+console.log(132 > 100 && 45 < 12 );
+console.log(34 > 33 && 23 < 90 );
+console.log(99 > 100 && 45 > 12 );
+console.log(132 > 100 || 45 < 12 );
+console.log(111 > 11 || 45 < 111 );
+console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) );
+console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );
+console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) );
+console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));
+console.log(!!'-1');
+console.log(!!-1);
+console.log(!!'0');
+console.log(!!'null');
+console.log(!!'undefined');
+console.log(!!(3/'owu'));
+console.log((111 > 11 || 45 < 111) ||  !!'0');
+console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
